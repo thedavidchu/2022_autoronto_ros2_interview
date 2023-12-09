83bece7fed84768965b3fecec944897948b8058e
Perception Cart <pcart@autodrive.utoronto.ca>
2023-12-09 15:05:44 -0500

    Fix run.sh script to be more self-contained

--- run.sh
+++ run.sh
@@ -1,19 +1,14 @@
 # Bring 'ros2' command into scope
-source /opt/ros/humble/setup.bash;
 
-# Check dependencies
-cd ~/dev_ws/src/autoronto_interview/;
-rosdep install -i --from-path src --rosdistro humble -y;
+cd ../../
 
 # Build package
-colcon build --packages-select autoronto_interview;
+colcon build --packages-select autoronto_interview
 
 # Source the setup files
-. install/setup.bash;
+./install/setup.bash
 
 # Run
-if [[ $1 == "solution" ]]; then
-    ros2 run autoronto_interview solution;
-else
-    ros2 run autoronto_interview davidchus_test;
-fi
+ros2 run autoronto_interview davidchus_test &
+ros2 run autoronto_interview solution
+
